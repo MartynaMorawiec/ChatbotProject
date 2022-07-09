ac813c6b3803267f362fa682b22b19577f14ca8b
Martyna <martyna.jasinska1@gmail.com>
2022-07-09 20:50:18 +0200

    Refactoring

--- chatbot/src/components/ChatbotFooter.jsx
+++ chatbot/src/components/ChatbotFooter.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
           <i className="fa-solid fa-paper-plane ml-2 mr-2 mb-2 text-[2rem] sm:text-[2.5rem] text-secondary hover:text-primary rotate-[40deg]"></i>
         </button>
       </form>
-      <button className="flex justify-center items-center w-[3.5em] h-[3.5em] ml-2 mr-2 mb-7 sm:w-[4em] sm:h-[4em] mt-[1em] bg-secondary hover:bg-primary text-white text-sm rounded-full shadow-blue">
+      <button className="flex justify-center items-center w-[3.5em] h-[3.5em] sm:w-[4em] sm:h-[4em] ml-2 mr-2 mb-7 mt-[1em] bg-secondary hover:bg-primary text-white text-sm rounded-full shadow-blue">
         <i className="fa-solid fa-microphone p-6 text-2xl sm:text-3xl"></i>
         {/* <i className="fa-solid fa-microphone-slash"></i> */}
       </button>
