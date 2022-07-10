8cb859e8ce43f2fbf9d5a277ab930473cf1a8c2f
Martyna <martyna.jasinska1@gmail.com>
2022-07-10 12:43:10 +0200

    Content props corrected

--- chatbot/src/components/ChatbotContent.jsx
+++ chatbot/src/components/ChatbotContent.jsx
@@ -5,26 +5,32 @@ import Loading from "./Loading";
 const ChatbotContent = () => {
   return (
     <div className="max-h-[calc(100vh-270px)] mt-[185px] mb-[100px] space-y-4 overflow-auto">
-      <Message actor="bot" type="text" content="Hello 🤚" />
+      <Message actor="bot" type="text" content={{ text: "Hello 🤚" }} />
       <Loading time={2} />
-      <Message actor="user" type="text" content="Yo Yo !!!" />
+      <Message actor="user" type="text" content={{ text: "Yo Yo !!!" }} />
       <Message
         actor="bot"
         type="card"
-        content={[
-          "https://images.theconversation.com/files/370685/original/file-20201123-13-x1rq79.jpg?ixlib=rb-1.1.0&rect=8%2C0%2C5422%2C3628&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
-          "Jujubes bonbon cookie danish pudding chocolate I love bonbon tart.",
-          "https://www.google.pl/",
-        ]}
+        content={{
+          image:
+            "https://images.theconversation.com/files/370685/original/file-20201123-13-x1rq79.jpg?ixlib=rb-1.1.0&rect=8%2C0%2C5422%2C3628&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+          text: "Jujubes bonbon cookie danish pudding chocolate I love bonbon tart.",
+          link: "https://www.google.pl/",
+        }}
       />
       <Message
         actor="bot"
         type="image"
-        content={
-          "https://media.istockphoto.com/photos/frozen-fire-picture-id1208302164?k=20&m=1208302164&s=170667a&w=0&h=u_rX3kg_XeHY_GsNyoo9nZjObQK0XmqZF2M5amurwZQ="
-        }
+        content={{
+          image:
+            "https://media.istockphoto.com/photos/frozen-fire-picture-id1208302164?k=20&m=1208302164&s=170667a&w=0&h=u_rX3kg_XeHY_GsNyoo9nZjObQK0XmqZF2M5amurwZQ=",
+        }}
       />
-      <Message actor="bot" type="link" content={"https://www.google.pl/"} />
+      <Message
+        actor="bot"
+        type="link"
+        content={{ link: "https://www.google.pl/" }}
+      />
       <Loading time={2} />
       <Message />
     </div>
--- chatbot/src/components/Message.jsx
+++ chatbot/src/components/Message.jsx
@@ -8,7 +8,7 @@ const Message = ({ actor, type, content }) => {
       {actor === "user" && type === "text" && (
         <section className="flex justify-end">
           <div className="max-w-[75%] bg-primary rounded-3xl rounded-br-md p-4 text-white text-base sm:text-lg">
-            {content}
+            {content.text}
             <div className="float-right mt-3 text-xs px-4 text-neutral-200">
               11:55
             </div>
@@ -19,7 +19,7 @@ const Message = ({ actor, type, content }) => {
       {actor == "bot" && type === "text" && (
         <section className="flex">
           <div className="max-w-[75%] bg-neutral-100 rounded-3xl rounded-tl-lg p-4 text-base sm:text-lg text-neutral-900">
-            {content}
+            {content.text}
             <div className="float-right mt-3 px-4 text-xs text-neutral-500">
               11:55
             </div>
@@ -29,12 +29,12 @@ const Message = ({ actor, type, content }) => {
 
       {actor == "bot" && type === "card" && (
         <section className="flex flex-col max-w-[50%] rounded-2xl overflow-hidden">
-          <img src={content[0]} />
+          <img src={content.image} />
           <p className="bg-neutral-100 text-sm p-3 leading-tight">
-            {content[1]}
+            {content.text}
           </p>
           <button className="py-2 bg-neutral-200 hover:bg-primary text-base text-neutral-900  hover:text-white">
-            <a href={content[2]} target="_blank">
+            <a href={content.link} target="_blank">
               Learn more
             </a>
           </button>
@@ -43,14 +43,14 @@ const Message = ({ actor, type, content }) => {
 
       {actor == "bot" && type === "image" && (
         <section className="flex flex-col max-w-[50%] rounded-2xl overflow-hidden">
-          <img src={content} />
+          <img src={content.image} />
         </section>
       )}
 
       {actor == "bot" && type === "link" && (
         <section className="flex flex-col w-6/12 rounded-2xl overflow-hidden">
           <button className="bg-neutral-100 hover:bg-primary text-base sm:text-lg py-2 text-secondary hover:text-white focus:shadow-inner">
-            <a href={content} target="_blank" className="underline">
+            <a href={content.link} target="_blank" className="underline">
               Click here
             </a>
           </button>
@@ -63,7 +63,7 @@ const Message = ({ actor, type, content }) => {
 Message.propTypes = {
   actor: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
-  content: PropTypes.any.isRequired,
+  content: PropTypes.object,
 };
 
 export default Message;
