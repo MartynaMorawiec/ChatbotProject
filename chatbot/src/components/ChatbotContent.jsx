063a99786c3b1ca0c07f16dfd24b81cba95290c7
Martyna <martyna.jasinska1@gmail.com>
2022-07-12 12:36:10 +0200

    chatMessages added to ChatbotContent

--- chatbot/src/components/ChatbotContent.jsx
+++ chatbot/src/components/ChatbotContent.jsx
@@ -2,34 +2,70 @@ import React from "react";
 import Message from "./Message";
 import Loading from "./Loading";
 
+const chatMessages = [
+  {
+    id: 1,
+    actor: "user",
+    content: {
+      text: "Hello",
+    },
+  },
+  {
+    id: 2,
+    actor: "bot",
+    content: {
+      text: "Good Morning",
+    },
+  },
+  {
+    id: 3,
+    actor: "bot",
+    content: {
+      text: "Have you already checked latest news?",
+      image:
+        "https://media.istockphoto.com/photos/villefranche-on-sea-in-evening-picture-id1145618475?k=20&m=1145618475&s=612x612&w=0&h=_mC6OZt_eWENYUAZz3tLCBTU23uvx5beulDEZHFLsxI=",
+      link: "https://nytimes.com/news",
+    },
+  },
+];
+
 const ChatbotContent = () => {
+  // const [messages, setMessages] = useState([]);
   return (
     <div className="max-h-[calc(100vh-270px)] mt-[185px] mb-[100px] space-y-4 overflow-auto">
-      <Message actor="bot" type="text" content={{ text: "Hello 🤚" }} />
-      <Loading time={2} />
-      <Message actor="user" type="text" content={{ text: "Yo Yo !!!" }} />
       <Message
-        actor="bot"
+        actor={chatMessages[1].actor}
+        type="text"
+        content={{ text: chatMessages[1].content.text }}
+      />
+      <Loading time={2} />
+      <Message
+        actor={chatMessages[0].actor}
+        type="text"
+        content={{ text: chatMessages[0].content.text }}
+      />
+      <Message
+        actor={chatMessages[2].actor}
         type="card"
         content={{
-          image:
-            "https://images.theconversation.com/files/370685/original/file-20201123-13-x1rq79.jpg?ixlib=rb-1.1.0&rect=8%2C0%2C5422%2C3628&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
-          text: "Jujubes bonbon cookie danish pudding chocolate I love bonbon tart.",
-          link: "https://www.google.pl/",
+          image: chatMessages[2].content.image,
+          text: chatMessages[2].content.text,
+          link: chatMessages[2].content.link,
         }}
       />
       <Message
-        actor="bot"
+        actor={chatMessages[2].actor}
         type="image"
         content={{
-          image:
-            "https://media.istockphoto.com/photos/frozen-fire-picture-id1208302164?k=20&m=1208302164&s=170667a&w=0&h=u_rX3kg_XeHY_GsNyoo9nZjObQK0XmqZF2M5amurwZQ=",
+          image: chatMessages[2].content.image,
         }}
       />
       <Message
-        actor="bot"
+        actor={chatMessages[2].actor}
         type="link"
-        content={{ link: "https://www.google.pl/" }}
+        content={{
+          link: chatMessages[2].content.link,
+        }}
       />
       <Loading time={2} />
     </div>
